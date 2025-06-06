27f408bc675d70e2dec74308e163760e144995a2
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-06 18:37:26 +0200

    feat(milestone 2): add other passenger data input fields, add output fields and switched output logic to them

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,14 +22,46 @@
         // Get ticket form DOM element
         const ticketFormElement = document.getElementById('ticket-form');
 
-        // Get kilometers number DOM element
-        const kilometersNumberElement = document.getElementById('kilometers-number');
+        //#region Input elements
+        
+            // Get kilometers number input DOM element
+            const kilometersNumberInputElement = document.getElementById('kilometers-number-input');
 
-        // Get age DOM element
-        const ageElement = document.getElementById('age');
+            // Get passenger age input DOM element
+            const passengerAgeInputElement = document.getElementById('passenger-age-input');
 
-        // Get submit button DOM element
-        const submitButtonElement = document.getElementById('submit-button');
+            // Get passenger name input DOM element
+            const passengerNameInputElement = document.getElementById('passenger-name-input');
+            
+            // Get passenger surname input DOM element
+            const passengerSurnameInputElement = document.getElementById('passenger-surname-input');
+            
+            // Get submit button DOM element
+            const submitButtonElement = document.getElementById('submit-button');
+            
+        //#endregion Input elements
+
+        //#region Output elements
+
+            // Get passenger name output DOM element
+            const passengerNameOutputElement = document.getElementById('passenger-name-output');
+
+            // Get passenger surname output DOM element
+            const passengerSurnameOutputElement = document.getElementById('passenger-surname-output');
+        
+            // Get passenger age output DOM element
+            const passengerAgeOutputElement = document.getElementById('passenger-age-output');
+
+            // Get kilometers number output DOM element
+            const kilometersNumberOutputElement = document.getElementById('kilometers-number-output');
+
+            // Get applied discounts DOM element
+            const appliedDiscountsElement = document.getElementById('applied-discounts');
+
+            // Get ticket price DOM element
+            const ticketPriceElement = document.getElementById('ticket-price');
+
+        //#endregion Output elements
 
     // #endregion DOM elements
 
@@ -45,24 +77,39 @@
 
         // #region Ticket calculation logic
 
-            // Get user entered kilometers number
-            const kilometersNumber = kilometersNumberElement.value;
+            // Get passenger entered name
+            const passengerName = passengerNameInputElement.value;
 
-            // Get user entered age
-            const age = ageElement.value;
+            // Get passenger entered surname
+            const passengerSurname = passengerSurnameInputElement.value;
+
+            // Get passenger entered age
+            const passengerAge = passengerAgeInputElement.value;
+
+            // Get passenger entered kilometers number
+            const kilometersNumber = kilometersNumberInputElement.value;
+
+            // Set default discount to none
+            let appliedDiscounts = 'nessuno';
 
             // Calculate total travel price
             let totalTravelPrice = kilometersNumber * KM_PRICE;
 
             // IF passenger age is underage
-            if ( age < UNDERAGE_AGE ) {
+            if ( passengerAge < UNDERAGE_AGE ) {
+
+                // Set applied discount to underage promotion
+                appliedDiscounts = `minorenne (${UNDERAGE_DISCOUNT_PERCENTAGE} %)`;
 
                 // Apply underage discount
                 totalTravelPrice = totalTravelPrice - ( (totalTravelPrice / 100 ) * UNDERAGE_DISCOUNT_PERCENTAGE );
 
             }
             // ELSE IF passenger age is senior 
-            else if ( age > SENIOR_AGE ) {
+            else if ( passengerAge > SENIOR_AGE ) {
+
+                // Set applied discount to senior promotion
+                appliedDiscounts = `senior (${SENIOR_DISCOUNT_PERCENTAGE} %)`;
 
                 // Apply senior discount
                 totalTravelPrice = totalTravelPrice - ( (totalTravelPrice / 100 ) * SENIOR_DISCOUNT_PERCENTAGE );
@@ -71,7 +118,7 @@
 
         // #endregion Ticket calculation logic
 
-        // #region Output section
+        // #region Ticket price formatting
 
             // Price rounding
             totalTravelPrice = totalTravelPrice.toFixed(2);
@@ -79,8 +126,27 @@
             // Price formatting for italian comma price format
             totalTravelPrice = totalTravelPrice.toString().replace('.', ',');
 
+        // #endregion Ticket price formatting
+
+        // #region Output section
+
+            // Passenger name output
+            passengerNameOutputElement.innerText = `Nome: ${passengerName}`;
+
+            // Passenger surname output
+            passengerSurnameOutputElement.innerText = `Cognome: ${passengerSurname}`;
+
+            // Passenger age output
+            passengerAgeOutputElement.innerText = `Età passeggero: ${passengerAge} anni`;
+
+            // Kilometers number output
+            kilometersNumberOutputElement.innerText = `Numero di km: ${kilometersNumber} km`;
+
+            // Applied discounts output
+            appliedDiscountsElement.innerText = 'Sconti applicati: ' + appliedDiscounts;
+
             // Price output
-            console.log('Il prezzo totale del viaggio è di: ' + totalTravelPrice + " €");
+            ticketPriceElement.innerText = 'Prezzo totale del viaggio: ' + totalTravelPrice + " €";
 
         // #endregion Output section
 
